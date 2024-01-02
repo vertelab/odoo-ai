9d6c0a28a7c6a41c40a0e38995a3911c997d9320
Anders Wallenquist <anders.wallenquist@vertel.se>
2024-01-02 16:56:09 +0000

    chat_ai_bot: better logging and base_url implemented

--- chat_ai_bot/models/mail_thread.py
+++ chat_ai_bot/models/mail_thread.py
@@ -29,15 +29,13 @@ class ResUsers(models.Model):
     openai_assistant_instructions = fields.Text(required=False)
     openai_assistant_tools = fields.Json(required=False)
     openai_assistant_model = fields.Char(required=False)
+    openai_assistant = fields.Char(required=False)
     
-    def get_stock_price(self,symbol: str) -> float:
-        stock = yf.Ticker(symbol)
-        price = stock.history(period="1d")['Close'].iloc[-1]
-        return price
 
 
 
-    def ai_send_message(self,openai_client,channel,run_instr,message):
+
+    def ai_send_message(self,openai_client,channel,message,run_instr=''):
 
         tools_list = [{
             "type": "function",
@@ -58,104 +56,32 @@ class ResUsers(models.Model):
             }
         }]
 
+        if message == '/reset':
+            thread_ids = self.env['openai.thread'].search([('channel','=',channel)])
+            if len(thread_ids)>0:
+                for thread in thread_ids:
+                    thread.unlink_thread(openai_client)
+            return []
+                    
 
 
-        thread = self.env['openai.thread'].browse(channel)
-        if not thread:
+        thread_ids = self.env['openai.thread'].search([('channel','=',channel)])        
+        _logger.warning(f"Thread {thread_ids=} {channel=}")
+        if len(thread_ids)==0:
             thread = self.env['openai.thread'].create({'channel': channel})
-            thread.thread_init(openai_client,
+            _logger.warning(f"Thread after {thread=} {channel=}")
+            thread.thread_init(openai_client,self,
                             self.openai_assistant_name or "Data Analyst Assistant",
                             self.openai_assistant_instructions or "You are a personal Data Analyst Assistant",
                             self.openai_assistant_tools or tools_list,
-                            self.openai_assistant_model or 'gpt-4-1106-preview',
-                            self.openai_api_key or "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",
-                            self.openai_base_url)
-        
-        
+                            self.openai_assistant_model or 'gpt-4-1106-preview',)
+        else:
+            thread = thread_ids[0]
+        _logger.warning(f"Thread ccc {thread.thread=} {thread.assistant=}")
 
-
-        # Step 3: Add a Message to a Thread
-        try:
-            message = openai_client.beta.threads.messages.create(
-                thread_id=thread.thread,
-                role="user",
-                content=message
-            )
-        except openai.APIConnectionError as e:
-            _logger.warning(f"OPENAI: The server could not be reached {e.__cause__}")
-        except openai.RateLimitError as e:
-            _logger.warning(f"OPENAI: Ratelimit {e.status_code} {e.response}")
-        except openai.APIStatusError as e:
-            _logger.warning(f"OPENAI: Status error {e.status_code} {e.response}")
-        
-        
-        
-
-        # Step 4: Run the Assistant
-        run = openai_client.beta.threads.runs.create(
-            thread_id=thread.thread,
-            assistant_id=thread.assistant,
-            instructions="Please address the user as Mervin Praison."
-        )
-
-        _logger.warning(f"Model_dump: {run.model_dump_json(indent=4)}")
-        msgs = []
-        while True:
-            # Wait for 5 seconds
-            time.sleep(1)
-
-            # Retrieve the run status
-            run_status = openai_client.beta.threads.runs.retrieve(
-                thread_id=thread.thread,
-                run_id=run.id
-            )
-            _logger.warning(f"Model_dump: {run.model_dump_json(indent=4)}")
-
-            # If run is completed, get messages
-            if run_status.status == 'completed':
-                messages = openai_client.beta.threads.messages.list(
-                    thread_id=thread.thread
-                )
-
-                # Loop through messages and print content based on role
-                for msg in messages.data:
-                    msgs.append({'role':msg.role,'content':msg.content[0].text.value})
-                    _logger.warning(f"{msg.role.capitalize()}: {msg.content=}")
-                return msgs
-                break
-            elif run_status.status == 'requires_action':
-                _logger.warning(f"Function calling")
-
-                required_actions = run_status.required_action.submit_tool_outputs.model_dump()
-                _logger.warning(f"{required_actions}")
-
-                tool_outputs = []
-                import json
-                for action in required_actions["tool_calls"]:
-                    func_name = action['function']['name']
-                    arguments = json.loads(action['function']['arguments'])
-                    _logger.warning(f"{arguments=}")
-                 
-                    
-                    if func_name == "get_stock_price":
-                        output = self.get_stock_price(symbol=arguments['symbol'])
-                        tool_outputs.append({
-                            "tool_call_id": action['id'],
-                            "output": output
-                        })
-                    else:
-                        raise ValueError(f"Unknown function: {func_name}")
-
-                _logger.warning("Submitting outputs back to the Assistant...")                    
-                openai_client.beta.threads.runs.submit_tool_outputs(
-                    thread_id=thread.thread,
-                    run_id=run.id,
-                    tool_outputs=tool_outputs
-                )
-                
-            else:
-                _logger.warning("Waiting for the Assistant to process...")                    
-                time.sleep(1)
+          
+        thread.add_message(openai_client,message)
+        msgs = thread.wait4response(openai_client,run_instr=run_instr)
 
         return msgs
 
@@ -170,27 +96,44 @@ class MailThread(models.AbstractModel):
 
         obj = self.env[msg_vals['model']].browse(msg_vals['res_id'])
         recipient_ids = self.env['res.users'].search([('partner_id','in',(obj.channel_partner_ids-message.author_id).mapped('id'))])
-        _logger.warning(f"{recipient_ids=}")
+        _logger.warning(f"direkt efter {recipient_ids=}")
+        _logger.warning(f"{self._context=}")
+        
+        
         if not self._context.get('ai_send_message',False):
-            client = openai.OpenAI(api_key="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")
-            msgs = recipient_ids[0].ai_send_message(client,msg_vals['res_id'],html2plaintext(message.body).strip(),f"Please address the user as {recipient_ids[0].name}.")
-            _logger.warning(f"{msgs=}")
-            for msg in msgs:
-                if not msg['role'] == 'user':
-                    obj.with_context(ai_send_message=True,mail_create_nosubscribe=True).sudo().message_post(
-                        body=f"{msg['role'].capitalize()}: {msg['content']}",
-                        author_id=recipient_ids[0].id,
-                        message_type='comment',
-                        subtype_xmlid='mail.mt_comment'
-                    )
+            for recipient in recipient_ids:
+                if recipient.is_ai_bot:
+                    
+                    # ~ https://www.linkedin.com/pulse/run-background-process-odoo-multi-threading-ahmed-rashad-mba-/
+                    # ~ https://www.cybrosys.com/blog/the-significance-of-multi-threading-in-odoo-16
+                    # ~ with api.Environment.manage():
+                       # ~ new_cr = self.pool.cursor()
+                       # ~ self = self.with_env(self.env(cr=new_cr))
+                       # ~ moves = self.env['stock.move'].search([
+                           # ~ ('id', 'in', move_ids)]).with_env(self.env(cr=new_cr))
+                   # ~ # Perform calculations and updates for each move
+                       # ~ for move in moves:
+                           # ~ # Logic for calculations and updates (missing details)
+                       # ~ new_cr.commit()
+                       
+                    client = openai.OpenAI(api_key=recipient.openai_api_key, base_url=recipient.openai_base_url)
+                    msgs = recipient.ai_send_message(   client,
+                                                        msg_vals['res_id'],
+                                                        html2plaintext(message.body).strip(),
+                                                        run_instr=f"Please address the user as {message.author_id.name}."
+                                                    )
+                    _logger.warning(f"{msgs=}")
+                    for msg in msgs:
+                        if not msg['role'] == 'user':
+                            obj.with_context(ai_send_message=True,mail_create_nosubscribe=True).sudo().message_post(
+                                body=f"{msg['content']}",
+                                author_id=recipient.id,
+                                message_type='comment',
+                                subtype_xmlid='mail.mt_comment'
+                            )
         else:
-            _logger.warning(f"{recipient_ids[0].id=}")
-        
-        # ~ raise UserError(f'{message.model=},{message.author_id=} {obj.channel_partner_ids=} {recipient_ids[0].openai_api_key=}')
-        
-        
-        #message=mail.message(359,),{'email_add_signature': True, 'record_name': 'Marc Demo, Mitchell Admin', 'author_id': 3, 'author_guest_id': False, 'email_from': '"Mitchell Admin" <admin@yourcompany.example.com>', 'model': 'mail.channel', 'res_id': 17, 'body': 'sfsdfsd', 'subject': False, 'message_type': 'comment', 'parent_id': False, 'subtype_id': 1, 'partner_ids': set(), 'attachment_ids': []}
-        
+            _logger.warning(f"No context {recipient_ids=}")
+
         return res
         
     def Xmessage_post(self, *,
@@ -210,21 +153,177 @@ class OpenAIThread(models.TransientModel):
     channel = fields.Char(required=False)
     assistant = fields.Char(required=False)
     thread = fields.Char(required=False)
+    run = fields.Char(required=False)
+    recipient_id = fields.Many2one(comodel_name='res.users', string='Recipient')
+    
+    
 
     
-    def init_thread(self,client,assistant_name,assistant_instructions,assistant_tools,
-                         assistant_model='gpt-4-1106-preview',api_key=None,base_url=None):
-
-                             
-        self.assistant = client.beta.assistants.create(
-                name=assistant_name,
-                instructions=assistant_instructions,
-                tools=assistant_tools,
-                model=assistant_model,
-            ).id
-        
-        
-        # Step 2: Create a Thread
+    def thread_init(self,client,user,assistant_name,assistant_instructions,assistant_tools,
+                         assistant_model='gpt-4-1106-preview'):
+        _logger.warning(f"Thread Init {client=} {user=}")
+        if not user.openai_assistant:
+            
+            user.openai_assistant = self.assistant = client.beta.assistants.create(
+                    name=assistant_name,
+                    instructions=assistant_instructions,
+                    tools=assistant_tools,
+                    model=assistant_model,
+                ).id
+        else:
+            self.assistant = user.openai_assistant
+            
         self.thread = client.beta.threads.create().id
+        self.recipient_id = user.id
         
-    
+    def log(self,message,role='user',status_code=200):
+        self.env['openai.log'].create({'recipient_id':self.recipient_id.id,
+                                       'channel_id': self.channel, 
+                                       'assistant': self.assistant, 
+                                       'thread': self.thread,
+                                       'run':self.run,
+                                       'message': message,
+                                       'status_code': status_code,
+                                       'role': role})
+        
+        
+    def add_message(self,client,message,role='user'):
+        """
+            Add a Message to a Thread
+        """
+        self.log(message)
+        try:
+            message = client.beta.threads.messages.create(
+                thread_id=self.thread,
+                role="user",
+                content=message
+            )
+        except openai.APIConnectionError as e:
+            _logger.warning(f"OPENAI: Thread The server could not be reached {e.__cause__}")
+            self.log(f"OPENAI: Thread The server could not be reached {e.__cause__}",status_code=e.status_code,role='openai')
+        except openai.RateLimitError as e:
+            _logger.warning(f"OPENAI: Thread Ratelimit {e.status_code} {e.response}")
+            self.log(f"OPENAI: Thread Ratelimit {e.status_code} {e.response}",status_code=e.status_code,role='openai')
+        except openai.APIStatusError as e:
+            _logger.warning(f"OPENAI: Thread Status error {e.status_code} {e.response}")
+            self.log(f"OPENAI: Thread Status error {e.status_code} {e.response}",status_code=e.status_code,role='openai',)
+        
+    def wait4response(self,client,run_instr=''):
+
+        if self.run:
+            run_status = client.beta.threads.runs.retrieve(
+                thread_id=self.thread,
+                run_id=self.run
+            )
+            if run_status.status == 'expired':
+                self.run = None
+                self.log(f"OPENAI: Status error run expired {self.run=} {run_status.status=}",status_code=400,role='openai',)
+        else:
+            _logger.warning(f"Run saved {self.run=}")
+            self.run = None
+                 
+        if not self.run:
+            try:
+                run = client.beta.threads.runs.create(
+                    thread_id=self.thread,
+                    assistant_id=self.assistant,
+                    instructions=run_instr or f"Please address the user as {self.recipient_id.name}."
+                )
+                self.log(run.model_dump_json(indent=4),role='run')
+                _logger.warning(f"Model_dump: {run.model_dump_json(indent=4)} {run.id=}")
+                self.run = run.id
+            except openai.APIConnectionError as e:
+                _logger.warning(f"OPENAI: Run The server could not be reached {e.__cause__}")
+                self.log(f"OPENAI: Run The server could not be reached {e.__cause__}",status_code=e.status_code,role='openai')
+                return []
+            except openai.RateLimitError as e:
+                _logger.warning(f"OPENAI: Run Ratelimit {e.status_code} {e.response}")
+                self.log(f"OPENAI: Run Ratelimit {e.status_code} {e.response}",status_code=e.status_code,role='openai')
+                return []
+            except openai.APIStatusError as e:
+                _logger.warning(f"OPENAI: Run Status error {e.status_code} {e.response}")
+                self.log(f"OPENAI: Run Status error {e.status_code} {e.response}",status_code=e.status_code,role='openai',)
+                return [{'role': 'assistant','content': f"OPENAI: Status error {e.status_code} {e.response}" }]
+                
+        msgs = []
+        while True:
+            # Wait for 5 seconds
+            time.sleep(1)
+
+            # Retrieve the run status
+            run_status = client.beta.threads.runs.retrieve(
+                thread_id=self.thread,
+                run_id=self.run
+            )
+            
+            self.log(f"{run_status.status=}",role='run',)
+            # If run is completed, get messages
+            if run_status.status == 'completed':
+                messages = client.beta.threads.messages.list(
+                    thread_id=self.thread
+                )
+                _logger.warning(f"Completed: {messages=}")
+
+                # Loop through messages and print content based on role
+                for msg in messages.data:
+                    self.log(' '.join([m.text.value for m in msg.content]),msg.role)
+                    if msg.run_id == self.run:
+                        msgs.append({'role':msg.role,'content':msg.content[0].text.value})
+                    _logger.warning(f"{msg.id=} {msg.run_id=} {msg.role=}: {msg.content=}")
+                self.run = None
+                break
+            elif run_status.status == 'requires_action':
+                
+                _logger.warning(f"Function calling")
+
+                required_actions = run_status.required_action.submit_tool_outputs.model_dump()
+                _logger.warning(f"{run_status.status=} {required_actions}")
+                self.log(f"{run_status.status=} {required_actions=}",role='run')
+                tool_outputs = []
+                import json
+                for action in required_actions["tool_calls"]:
+                    func_name = action['function']['name']
+                    arguments = json.loads(action['function']['arguments'])
+                    _logger.warning(f"{arguments=}")
+                 
+                    
+                    if func_name == "get_stock_price":
+                        output = self.get_stock_price(symbol=arguments['symbol'])
+                        tool_outputs.append({
+                            "tool_call_id": action['id'],
+                            "output": output
+                        })
+                    else:
+                        raise ValueError(f"Unknown function: {func_name}")
+
+                _logger.warning("Submitting outputs back to the Assistant...")
+                self.log(f"Submit tools outputs {tool_outputs=}",role='run')
+                client.beta.threads.runs.submit_tool_outputs(
+                    thread_id=self.thread,
+                    run_id=self.run,
+                    tool_outputs=tool_outputs
+                )
+                
+            else:
+                _logger.warning(f"Waiting for the Assistant to process... {run_status.status=} {self.run=}") 
+                                   
+                time.sleep(1)
+        return msgs
+
+
+    def unlink_thread(self,client):
+        try:
+            client.beta.assistants.delete(self.assistant)
+        except openai.APIConnectionError as e:
+            _logger.warning(f"OPENAI: Delete The server could not be reached {e.__cause__}")
+        except openai.RateLimitError as e:
+            _logger.warning(f"OPENAI: Delete Ratelimit {e.status_code} {e.response}")
+        except openai.APIStatusError as e:
+            _logger.warning(f"OPENAI: Delete Status error {e.status_code} {e.response}")
+        self.unlink()
+            
+            
+    def get_stock_price(self,symbol: str) -> float:
+        stock = yf.Ticker(symbol)
+        price = stock.history(period="1d")['Close'].iloc[-1]
+        return price
--- chat_ai_bot/models/openai_log.py
+++ chat_ai_bot/models/openai_log.py
@@ -6,48 +6,26 @@ from odoo import models, fields, _
 class OpenaiLog(models.Model):
     _name = 'openai.log'
     _description = 'OpenAI Log'
-
+    _order = 'id desc'
 
     date = fields.Datetime(
         string='Date',
         help="""Date""",
         required=True,
-        default=fields.Datetime.now(),
+        default=fields.Datetime.now,
     )
-
-    type = fields.Selection(
-        string='Request Type',
-        help="""Type of request""",
-        selection=[
-            ('completition', 'Completition'),
-            ('chat-completion', 'Chat Completion'),
-        ],
-        required=True,
-    )
-
-
-    raw_request = fields.Text(
-        string='Raw Request',
-        help="""Request, as-is, sent to OpenAI Api""",
-        required=True,
-    )
-
-    raw_response = fields.Text(
-        string='Raw Response',
-        help="""Response, as-is, received from OpenAI Api""",
-        required=True,
-    )
-
+    channel_id = fields.Many2one(comodel_name='mail.channel', string='Channel')
+    assistant = fields.Char(required=False)
+    role = fields.Char(required=False)
+    status_code = fields.Integer(required=False)
     thread = fields.Char(string='Thread ID',help="Thread ID for the request")
-
-    parsed_request = fields.Text(
-        string='Parsed Request',
-        help="""Parsed Request, to be human readable""",
-        required=False,
+    run = fields.Char(string='Run ID',help="Run ID for the request")
+    recipient_id = fields.Many2one(comodel_name='res.users', string='Recipient')
+    
+    message = fields.Text(
+        string='Raw Request',
+        help="""Request/RESPONSE, as-is, sent to OpenAI Api""",
+        required=True,
     )
 
-    parsed_response = fields.Text(
-        string='Parsed Response',
-        help="""Parsed Response, to be human readable""",
-        required=False,
-    )
+    
