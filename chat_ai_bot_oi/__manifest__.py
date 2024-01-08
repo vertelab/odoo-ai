5d0bd870961d8cf0d72d3784f81b725b755be73d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-01-08 11:13:30 +0100

    Update __manifest__.py
    preparing for apps.odoo.com

--- chat_ai_bot_oi/__manifest__.py
+++ chat_ai_bot_oi/__manifest__.py
@@ -1,14 +1,45 @@
 # -*- coding: utf-8 -*-
-{
-    'name': 'Chat AI-Bot for Open Interpreter',
-    'summary': 'AI Bot for LLM-servers compatible with Open Interpreter',
-    'description': """AI Bot for LLM-servers compatible with Open Interpreter """,
-    'license': 'AGPL-3',
-    'version': '16.0.1.0.3',
-    'category': 'Productivity/Discuss',
-    'author': 'Vertel AB',
-    'website': 'https://vertel.se',
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
+{
+    'name': 'Chat AI-Bot: for Open Interpreter',
+    'version': '16.01.0.3',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'AI Bot for LLM-servers compatible with Open Interpreter',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity / Discuss',
+    'description': """
+    AI Bot for LLM-servers compatible with Open Interpreter.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-ai/chat_ai_bot_oi',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-ai',
+    # Any module necessary for this one to work correctly
     'depends': [
         'chat_ai_bot_common'
     ],
@@ -17,15 +48,10 @@
             'open-interpreter'
         ]
     },
-
     'data': [
     ],
-
-#    'images': [
-#        'static/description/cover/odoogpt.png',
-#    ],
-
     'installable': True,
     'auto_install': False,
     'application': False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
