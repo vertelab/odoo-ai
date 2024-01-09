6781a59c9491a1dc1409a27a21d59730ef98f547
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-01-09 10:43:23 +0100

    Update __manifest__.py

--- chat_ai_bot_openai/__manifest__.py
+++ chat_ai_bot_openai/__manifest__.py
@@ -18,7 +18,6 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 {
     'name': 'Chat AI-Bot: for OpenAI',
     'version': '16.01.0.3',
