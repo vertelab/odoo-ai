4306374df44cf56cd6daa7c1ad5836658bf009e5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-01-09 10:44:59 +0100

    Update __manifest__.py

--- chat_ai_bot_common/__manifest__.py
+++ chat_ai_bot_common/__manifest__.py
@@ -1,4 +1,4 @@
--Bot# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
