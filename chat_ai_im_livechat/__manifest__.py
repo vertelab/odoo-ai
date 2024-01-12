8fefd6739c65d0519078b62e32927c1bbe82ea9f
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-01-12 10:52:30 +0100

    Create __manifest__.py

---
+++ chat_ai_im_livechat/__manifest__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Chat AI-Bot: IM LiveChat',
+    'version': '16.01.0.3',
+    # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'AI Bot for LLM-servers compatible with OpenAI/using openai-ap',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Productivity / Discuss',
+    'description': """
+    To be installed and used at the customers website and will AI-respond to visitors interaction.
+    This IM LiveChat needs to be tuned to the correct topic.
+
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-ai/chat_ai_im_livechat',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-ai',
+    # Any module necessary for this one to work correctly
+    'depends': [
+        'chat_ai_bot_common'
+    ],
+    'external_dependencies': {
+        'python': [
+            'openai'
+        ]
+    },
+
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
