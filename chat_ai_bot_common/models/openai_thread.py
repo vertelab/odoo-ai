3fb8d0d25fba9572b390245029dec1b76b4370e4
iammiracle <miracleaayodele@gmail.com>
2024-01-18 14:09:50 +0100

    pickle pickle

--- chat_ai_bot_common/models/openai_thread.py
+++ chat_ai_bot_common/models/openai_thread.py
@@ -94,5 +94,5 @@ class OpenAIThread(models.TransientModel):
         return pickle.loads(base64.decode(self.client))
 
     def client_init(self, client):
-        self.client = base64.encode(pickle.dumps(client.read()))
+        # self.client = base64.encode(pickle.dumps(client.read()))
         self.client = pickle.dumps(client.read())
