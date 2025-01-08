45dfb3d515de86a41020b1950ee7838c5eacd5a5
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-01-08 10:38:43 +0100

    Update __manifest__.py

--- chat_ai_bot_perplexity/__manifest__.py
+++ chat_ai_bot_perplexity/__manifest__.py
@@ -31,7 +31,7 @@
     """,
     #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-ai/chat_ai_bot_perplex',
+    'website': 'https://vertel.se/apps/odoo-ai/chat_ai_bot_perplexity',
     'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
