af8de53ba38d78911368ac3855f99451b13a07a0
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-01-08 10:18:10 +0100

    Update __manifest__.py

--- chat_ai_bot_openai/__manifest__.py
+++ chat_ai_bot_openai/__manifest__.py
@@ -18,7 +18,7 @@
 #
 ##############################################################################
 {
-    'name': 'Chat AI-Bot: for OpenAI',
+    'name': 'odoo-ai: Chat AI-Bot: for OpenAI',
     'version': '1.3',
     # Version ledger: 16.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'AI Bot for LLM-servers compatible with OpenAI/using openai-ap',
