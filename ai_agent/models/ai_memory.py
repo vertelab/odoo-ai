ab1b5b96526c6ff5bc6a550f4886156451926283
Anders Wallenquist <anders.wallenquist@vertel.se>
2025-01-13 13:14:51 +0000

    ai_agent: memory new fields

--- ai_agent/models/ai_memory.py
+++ ai_agent/models/ai_memory.py
@@ -16,6 +16,8 @@ from odoo.exceptions import UserError, AccessError, ValidationError
 from odoo.tools.safe_eval import safe_eval
 from random import randint
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import asyncio
+from crawl4ai import AsyncWebCrawler
 
 from dateutil.relativedelta import relativedelta
 
@@ -31,6 +33,14 @@ class AIAgentMemory(models.Model):
     nbr_days = fields.Integer(string='Number days this memory will live', related="ai_memory_id.nbr_days")
     last_run = fields.Datetime(string='Last Run', related="ai_memory_id.last_run")
     ai_memory_id = fields.Many2one(comodel_name='ai.memory', string="Memory", help="")
+    ai_memory_status = fields.Selection(selection=[("draft", _("Draft")), ("active", _("Active")), ("done", _("Done")), ("error", _("Error"))],related="ai_memory_id.status")
+    ai_memory_llm_is = fields.Many2one(comodel_name='',string="",help="") # domain|context|ondelete="'set null', 'restrict', 'cascade'"|auto_join|delegate  fields.Char(string='Url', related="ai_memory_id.url" )
+    ai_memory_llm_id = fields.Many2one(comodel_name="ai.agent.llm", string="LLM", related="ai_memory_id.llm_id")
+    ai_memory_llm_status = fields.Selection(selection=[("not_confirmed", "Not Confirmed"), ("confirmed", "Confirmed"), ("error", "Error")],related="ai_memory_id.llm_id.status")
+    ai_memory_url = fields.Char(string='Url', related="ai_memory_id.url" )
+
+    def run(self):
+        self.ai_memory_id.run()
 
 
 class AIMemory(models.Model):
@@ -39,19 +49,22 @@ class AIMemory(models.Model):
 
     _description = 'AI Memory'
 
-    ai_type = fields.Selection(selection=[("default", "Default"), ('ai-programmer', 'AI Programmer')],
-                               default="default", required=True)
+    ai_type = fields.Selection(selection=[("default", "Default"), ('ai-programmer', 'AI Programmer')],default="default", required=True)
+    base_image_128 = fields.Image("Base Image", max_width=128, max_height=128, compute='_compute_base_image_128')
     color = fields.Integer(default=lambda self: randint(1, 11))
     debug = fields.Boolean(string='Debug')
+    image_128 = fields.Image("Image", max_width=128, max_height=128)
     is_favorite = fields.Boolean()
     last_run = fields.Datetime()
-    name = fields.Char(required=True)
-    tag_ids = fields.Many2many(comodel_name='product.tag', string='Tags')
-    image_128 = fields.Image("Image", max_width=128, max_height=128)
-    base_image_128 = fields.Image("Base Image", max_width=128, max_height=128, compute='_compute_base_image_128')
-    memory_type = fields.Selection(selection=[('faiss', 'FAISS'), ('st', 'Short Term')], string='Memory type')
+    llm_id = fields.Many2one(comodel_name="ai.agent.llm", string="LLM", help="Choose Embedded Large Language Model",domain="[('status','=','confirmed')]")
     memory_faiss = fields.Binary(string='FAISS Index', attachment=True)
+    memory_markdown = fields.Binary(string='Markdown', attachment=True)
+    memory_type = fields.Selection(selection=[('faiss', 'FAISS'), ('st', 'Short Term')], string='Memory type')
+    name = fields.Char(required=True)
     nbr_days = fields.Integer(string='Number days this memory will live')
+    status = fields.Selection(selection=[("draft", _("Draft")), ("active", _("Active")), ("done", _("Done")), ("error", _("Error"))],default="draft")
+    tag_ids = fields.Many2many(comodel_name='product.tag', string='Tags')
+    url = fields.Char(string='Url', trim=True, )
 
     @api.depends('image_128')
     def _compute_base_image_128(self):
@@ -104,8 +117,24 @@ class AIMemory(models.Model):
             self.status = "error"
         self.message_post(body=f"{body} | {self.last_run}", message_type="notification")
 
+
+# sudo apt install libgstreamer-gl1.0-0 libgstreamer-plugins-base1.0-0 libflite1 libavif-dev libharfbuzz-icu0 libenchant-2-2 libsecret-1-0 libhyphen0 libmanette-0.2-0 libgles2
+# npx playwright install --with-deps
+#playwright install
+# npx playwright install-deps chromium
+#aw@odoo16:~/.cache$ sudo cp -r ms-playwright /var/lib/odoo/.cache/
     def run(self):
         self.last_run = fields.Datetime.now()
+        if self.url:                
+            async def run_crawler():
+            # Create an instance of AsyncWebCrawler
+                async with AsyncWebCrawler() as crawler:
+                    # Run the crawler on a URL
+                    result = await crawler.arun(url=self.url)
+                    return result
+            # Use asyncio.run() to execute the async function in a synchronous manner
+            self.memory_markdown = asyncio.run(run_crawler())        
+        
 
     def cron(self):
         self.env['ai.memory'].search(
