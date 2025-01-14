82939399afcd72ee4875fba2b3a0f584f60236ad
marcuszxc <marcuszxc10@gmail.com>
2025-01-14 09:28:01 +0000

    Started working on raging models and added rag test wizard

--- ai_agent/models/ai_memory.py
+++ ai_agent/models/ai_memory.py
@@ -5,31 +5,26 @@ import PyPDF2
 import base64
 import uuid
 import faiss
+import asyncio
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.document_loaders import TextLoader
-# from langchain.embeddings import HuggingFaceEmbeddings
-# from langchain.llms import HuggingFacePipeline
 from langchain.memory import ConversationBufferMemory
 from langchain.schema import SystemMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
-from odoo import models, fields, api, _
-from odoo.tools.safe_eval import safe_eval
 from random import randint
-# from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents.base import Document
 from langchain_text_splitters.character import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from odoo.exceptions import UserError
-
-import asyncio
 from crawl4ai import AsyncWebCrawler
-
 from dateutil.relativedelta import relativedelta
 
-_logger = logging.getLogger(__name__)
+from odoo import models, fields, api, _
+from odoo.tools.safe_eval import safe_eval
+from odoo.exceptions import UserError
 
+_logger = logging.getLogger(__name__)
 
 class AIAgentMemory(models.Model):
     _name = 'ai.agent.memory'
@@ -56,7 +51,7 @@ class AIMemory(models.Model):
 
     _description = 'AI Memory'
 
-    ai_type = fields.Selection(selection=[("default", "Default"), ('ai-programmer', 'AI Programmer')],default="default", required=True)
+    ai_type = fields.Selection(selection=[("default", "Default"), ('ai-programmer', 'AI Programmer'), ('model_rag','Model RAG')],default="default", required=True)
     base_image_128 = fields.Image("Base Image", max_width=128, max_height=128, compute='_compute_base_image_128')
     color = fields.Integer(default=lambda self: randint(1, 11))
     debug = fields.Boolean(string='Debug')
@@ -75,6 +70,8 @@ class AIMemory(models.Model):
     status = fields.Selection(selection=[("draft", _("Draft")), ("active", _("Active")), ("done", _("Done")), ("error", _("Error"))],default="draft")
     tag_ids = fields.Many2many(comodel_name='product.tag', string='Tags')
     url = fields.Char(string='Url', trim=True, )
+    model_id = fields.Many2one(comodel_name='ir.model', )
+
 
     @api.depends('image_128')
     def _compute_base_image_128(self):
@@ -83,27 +80,55 @@ class AIMemory(models.Model):
 
     def rag_attatchemts(self):
         for memory in self:
-            documents = [self.create_document(attachment) for attachment in 
+            raw_documents = [self.create_document_from_file(attachment) for attachment in 
                          self.env["ir.attachment"].search([("res_model", "=", memory._name), ("res_id", "=", memory.id)])]
-            if documents:
-                all_splits = self.text_splitter().split_documents(documents)
+            if raw_documents:
+                documents = self.text_splitter(raw_documents)
                 self.create_faiss(documents)
             else:
                 raise UserError(_("No attachments to RAG"))
         
-    def load_faiss(self):
-        faiss_file = base64.b64decode(self.memory_faiss)
-        db = FAISS.deserialize_from_bytes(faiss_file,eval(self.ai_agent_llm_id.get_embedding()), allow_dangerous_deserialization=True)
-        return db
+    def rag_models(self):
+        for memory in self:
+            model_ids = memory.env["ir.model"].search([])
+            raw_documents = [self.create_document(model_id) for model_id in model_ids]
+            documents = self.text_splitter(raw_documents)
+            self.create_faiss(documents)
 
-    def text_splitter(self):
-        return RecursiveCharacterTextSplitter(
+    def action_test_rag(self):
+        _logger.error("runs"*100)
+        action = {
+            'name': 'Test Action',
+            'type': 'ir.actions.act_window',
+            'res_model': 'ai.memory.test.wizard',
+            'view_mode': 'form',
+            'context': {'default_ai_memory': self.id},
+            'target': 'new'
+        }
+        _logger.error(f"{action}")
+        return action
+
+
+    def load_faiss(self):
+        if self.memory_faiss:
+            faiss_file = base64.b64decode(self.memory_faiss)
+            db = FAISS.deserialize_from_bytes(faiss_file,eval(self.ai_agent_llm_id.get_embedding()), allow_dangerous_deserialization=True)
+            return db
+        else:
+            return False
+
+    def text_splitter(self,documents):
+        text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=self.split_chunk_size,  # chunk size (characters)
         chunk_overlap=self.split_chunk_overlap,  # chunk overlap (characters)
         add_start_index=True,  # track index in original document
     )
+        return text_splitter(documents)
 
-    def create_document(self, attachment_id):
+    def create_document(self,text):
+        pass
+
+    def create_document_from_file(self, attachment_id):
         content = base64.b64decode(attachment_id.datas).decode("utf-8")
         if attachment_id.name.split(".")[-1] == "pdf":
             reader = PyPDF2.PdfFileReader(file)
@@ -113,7 +138,6 @@ class AIMemory(models.Model):
         
     def create_faiss(self,documents):
         db = FAISS.from_documents(documents, eval(self.ai_agent_llm_id.get_embedding()))
-        _logger.error(f"{db.serialize_to_bytes()=}")
         self.memory_faiss = base64.b64encode(db.serialize_to_bytes())
 
 
@@ -137,23 +161,25 @@ class AIMemory(models.Model):
             self.status = "error"
         self.message_post(body=f"{body} | {self.last_run}", message_type="notification")
 
+    # def run(self):
+
 
 # sudo apt install libgstreamer-gl1.0-0 libgstreamer-plugins-base1.0-0 libflite1 libavif-dev libharfbuzz-icu0 libenchant-2-2 libsecret-1-0 libhyphen0 libmanette-0.2-0 libgles2
 # npx playwright install --with-deps
 #playwright install
 # npx playwright install-deps chromium
 #aw@odoo16:~/.cache$ sudo cp -r ms-playwright /var/lib/odoo/.cache/
-    def run(self):
-        self.last_run = fields.Datetime.now()
-        if self.url:                
-            async def run_crawler():
-            # Create an instance of AsyncWebCrawler
-                async with AsyncWebCrawler() as crawler:
-                    # Run the crawler on a URL
-                    result = await crawler.arun(url=self.url)
-                    return result
-            # Use asyncio.run() to execute the async function in a synchronous manner
-            self.memory_markdown = asyncio.run(run_crawler())        
+    # def run(self):
+    #     self.last_run = fields.Datetime.now()
+    #     if self.url:                
+    #         async def run_crawler():
+    #         # Create an instance of AsyncWebCrawler
+    #             async with AsyncWebCrawler() as crawler:
+    #                 # Run the crawler on a URL
+    #                 result = await crawler.arun(url=self.url)
+    #                 return result
+    #         # Use asyncio.run() to execute the async function in a synchronous manner
+    #         self.memory_markdown = asyncio.run(run_crawler())        
         
 
     def cron(self):
