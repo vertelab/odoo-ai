f83f53df96bf74197e38c2467778596be1074abc
marcuszxc <marcuszxc10@gmail.com>
2025-01-14 15:30:23 +0000

    ai_agent: Added RAG for models and modulse

--- ai_agent/models/ai_memory.py
+++ ai_agent/models/ai_memory.py
@@ -6,6 +6,10 @@ import base64
 import uuid
 import faiss
 import asyncio
+import requests
+import markdownify
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
 from langchain.chains import ConversationalRetrievalChain
 from langchain_community.document_loaders import TextLoader
 from langchain.memory import ConversationBufferMemory
@@ -44,6 +48,24 @@ class AIAgentMemory(models.Model):
     def run(self):
         self.ai_memory_id.run()
 
+class AIquestMemory(models.Model):
+    _name = 'ai.quest.memory'
+    _description = 'AI Quest Memory'
+
+    ai_quest_id = fields.Many2one(comodel_name='ai.quest', string="", help="")
+    sequence = fields.Integer(string='Sequence')
+    nbr_days = fields.Integer(string='Number days this memory will live', related="ai_memory_id.nbr_days")
+    last_run = fields.Datetime(string='Last Run', related="ai_memory_id.last_run")
+    ai_memory_id = fields.Many2one(comodel_name='ai.memory', string="Memory", help="")
+    ai_memory_status = fields.Selection(selection=[("draft", _("Draft")), ("active", _("Active")), ("done", _("Done")), ("error", _("Error"))],related="ai_memory_id.status")
+    ai_memory_llm_is = fields.Many2one(comodel_name='',string="",help="") # domain|context|ondelete="'set null', 'restrict', 'cascade'"|auto_join|delegate  fields.Char(string='Url', related="ai_memory_id.url" )
+    ai_memory_llm_id = fields.Many2one(comodel_name="ai.agent.llm", string="LLM", related="ai_memory_id.llm_id")
+    ai_memory_llm_status = fields.Selection(selection=[("not_confirmed", "Not Confirmed"), ("confirmed", "Confirmed"), ("error", "Error")],related="ai_memory_id.llm_id.status")
+    ai_memory_url = fields.Char(string='Url', related="ai_memory_id.url" )
+
+    def run(self):
+        self.ai_memory_id.run()
+
 
 class AIMemory(models.Model):
     _name = 'ai.memory'
@@ -51,7 +73,8 @@ class AIMemory(models.Model):
 
     _description = 'AI Memory'
 
-    ai_type = fields.Selection(selection=[("default", "Default"),],default="default", required=True)
+    memory_type = fields.Selection(selection=[("bs4", "Simple Webscraper"),("model","Model"),("module", "Module"),("local_attachment","Local Attachment"), ("object_attachment", "Object Attachment")],default="model", required=True, help="This is the source for memory")
+    vector_type = fields.Selection(selection=[('faiss', 'FAISS'), ('st', 'Short Term')], string='Vector type', help="The type of vector database")
     base_image_128 = fields.Image("Base Image", max_width=128, max_height=128, compute='_compute_base_image_128')
     color = fields.Integer(default=lambda self: randint(1, 11))
     debug = fields.Boolean(string='Debug')
@@ -61,7 +84,6 @@ class AIMemory(models.Model):
     llm_id = fields.Many2one(comodel_name="ai.agent.llm", string="LLM", help="Choose Embedded Large Language Model",domain="[('status','=','confirmed')]")
     memory_faiss = fields.Binary(string='FAISS Index', attachment=True)
     memory_markdown = fields.Binary(string='Markdown', attachment=True)
-    memory_type = fields.Selection(selection=[('faiss', 'FAISS'), ('st', 'Short Term')], string='Memory type')
     name = fields.Char(required=True)
     nbr_days = fields.Integer(string='Number days this memory will live')
     split_chunk_size = fields.Integer(default=1000)
@@ -69,8 +91,12 @@ class AIMemory(models.Model):
     ai_agent_llm_id = fields.Many2one(string="Embedded LLM", comodel_name="ai.agent.llm", required=True, domain="[('status','=','confirmed')]")
     status = fields.Selection(selection=[("draft", _("Draft")), ("active", _("Active")), ("done", _("Done")), ("error", _("Error"))],default="draft")
     tag_ids = fields.Many2many(comodel_name='product.tag', string='Tags')
+    attachment_ids = fields.Many2many(comodel_name="ir.attachment")
     url = fields.Char(string='Url', trim=True, )
-    model_id = fields.Many2one(comodel_name='ir.model', )
+    model_id = fields.Many2one(comodel_name='ir.model')
+    object_id = fields.Reference(string='Object',
+                                selection=lambda m: [(model.model, model.name) for model in
+                                                      m.env['ir.model'].sudo().search([])])
 
 
     @api.depends('image_128')
@@ -83,17 +109,23 @@ class AIMemory(models.Model):
             raw_documents = [self.create_document_from_file(attachment) for attachment in 
                          self.env["ir.attachment"].search([("res_model", "=", memory._name), ("res_id", "=", memory.id)])]
             if raw_documents:
-                documents = self.text_splitter(raw_documents)
-                self.create_faiss(documents)
+                self.create_faiss(raw_documents)
             else:
                 raise UserError(_("No attachments to RAG"))
         
     def rag_models(self):
         for memory in self:
-            model_ids = memory.env["ir.model"].search([])
-            raw_documents = [self.create_document(model_id) for model_id in model_ids]
-            documents = self.text_splitter(raw_documents)
-            self.create_faiss(documents)
+            model_dicts = memory.env["ir.model"].search([]).read(["name", "model", "info","modules"])
+            if len(model_dicts) != 0: 
+                raw_documents = [self.create_document(text=json.dumps(model_dict),metadata=model_dict) for model_dict in model_dicts]
+                self.create_faiss(raw_documents)
+
+    def rag_modules(self):
+        for memory in self:
+            module_dicts = memory.env["ir.module.module"].search([]).read(["name", "shortdesc", "summary", "description", "author", "maintainer"])
+            if len(module_dicts) != 0: 
+                raw_documents = [self.create_document(text=json.dumps(module_dict),metadata=module_dict) for module_dict in module_dicts]
+                self.create_faiss(raw_documents)
 
     def action_test_rag(self):
         _logger.error("runs"*100)
@@ -124,8 +156,8 @@ class AIMemory(models.Model):
         add_start_index=True,  # track index in original document
     ).split_documents(documents)
 
-    def create_document(self,text):
-        pass
+    def create_document(self,text,metadata):
+        return Document(id=uuid.uuid4(), page_content=text, metadata=metadata)
 
     def create_document_from_file(self, attachment_id):
         content = base64.b64decode(attachment_id.datas).decode("utf-8")
@@ -135,7 +167,8 @@ class AIMemory(models.Model):
         _logger.error(f"{content}")     
         return Document(id=uuid.uuid4(), page_content=f"{content}", metadata={"name": attachment_id.name, "type": "attachment"})
         
-    def create_faiss(self,documents):
+    def create_faiss(self,raw_documents):
+        documents = self.text_splitter(raw_documents)
         db = FAISS.from_documents(documents, eval(self.ai_agent_llm_id.get_embedding()))
         self.memory_faiss = base64.b64encode(db.serialize_to_bytes())
 
@@ -160,27 +193,56 @@ class AIMemory(models.Model):
             self.status = "error"
         self.message_post(body=f"{body} | {self.last_run}", message_type="notification")
 
-    # def run(self):
+    def run(self):
+        if self.status == "active":
+            self.last_run = fields.Datetime.now()
+            if self.memory_type == 'bs4' and self.url:
+                all_pages = self.scrape_website(self.url)
+                _logger.warning(f"scrape ended {len(all_pages)=} -----------------------------------------")
+                self.memory_markdown = base64.b64encode(self.scrape_website(self.url))
+            elif self.memory_type == 'model':
+                self.rag_models()
+            elif self.memory_type == "module":
+                self.rag_modules()
+            elif 'attachment' in self.memory_type:
+                self.rag_attatchemts()
+        else:
+            raise UserError(_(f"Wrong state on memory ({self.name})"))          
+            
+    def scrape_website(self,website):
+        self.ensure_one()
+        global all_pages
+        all_pages = ""
+        def is_same_domain(url, domain):
+            return urlparse(url).netloc == urlparse(domain).netloc
 
+        def scrape_page(url, visited):
+            global all_pages
+            _logger.warning(f'scraping {url=} {visited=} {len(all_pages)=}')
+            if url in visited:
+                return
+            visited.add(url)
+            
+            try:
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                
+                # Save page content as attachment
+                content = soup.get_text()
+                all_pages += markdownify.markdownify(content)
+                
+                # Follow links
+                for link in soup.find_all('a', href=True):
+                    next_url = urljoin(url, link['href'])
+                    if is_same_domain(next_url, website):
+                        scrape_page(next_url, visited)
+            except Exception as e:
+                _logger.error(f"Error scraping {url}: {str(e)}")
+        visited_urls = set()
+        scrape_page(website, visited_urls)
+        return all_pages.encode('utf-8')
 
-# sudo apt install libgstreamer-gl1.0-0 libgstreamer-plugins-base1.0-0 libflite1 libavif-dev libharfbuzz-icu0 libenchant-2-2 libsecret-1-0 libhyphen0 libmanette-0.2-0 libgles2
-# npx playwright install --with-deps
-#playwright install
-# npx playwright install-deps chromium
-#aw@odoo16:~/.cache$ sudo cp -r ms-playwright /var/lib/odoo/.cache/
-    # def run(self):
-    #     self.last_run = fields.Datetime.now()
-    #     if self.url:                
-    #         async def run_crawler():
-    #         # Create an instance of AsyncWebCrawler
-    #             async with AsyncWebCrawler() as crawler:
-    #                 # Run the crawler on a URL
-    #                 result = await crawler.arun(url=self.url)
-    #                 return result
-    #         # Use asyncio.run() to execute the async function in a synchronous manner
-    #         self.memory_markdown = asyncio.run(run_crawler())        
-        
-
+   
     def cron(self):
         self.env['ai.memory'].search(
             [('last_run', '<', fields.Datetime.now() - relativedelta(days=self.nbr_days))]).run()
