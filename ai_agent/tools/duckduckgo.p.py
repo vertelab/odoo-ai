788cb7a18ed8d9ee1af143bfff1b6a48e8d93622
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2025-01-22 09:52:02 +0100

    changes to getllm for azureopenai

--- ai_agent/tools/duckduckgo.p.py
+++ ai_agent/tools/duckduckgo.p.py
@@ -3,13 +3,13 @@ import requests
 from langchain.tools import tool
 from bs4 import BeautifulSoup
 from duckduckgo_search import DDGS
-
+from odoo.addons.ai_agent.models.ai_quest import AgentState as State
 _logger = logging.getLogger(__name__)
 
 @tool("internet_search_DDGO", return_direct=False)
-def internet_search_DDGO(query: str) -> str:
+def internet_search_DDGO(query: str, state:State) -> str:
     """Searches the internet using DuckDuckGo."""
-   
+    _logger.warning(f"{state=}")
     results = list(DDGS().text(query, max_results=5))
 
     return results if results else "No results found."
