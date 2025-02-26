52b927927de4e6b44eeb61ac4c8996f8fb7ca415
marcuszxc <marcuszxc10@gamil.com>
2025-02-26 12:12:56 +0000

    ai_agent: Synct files with p-files

--- ai_agent/__manifest__.py
+++ ai_agent/__manifest__.py
@@ -140,6 +140,16 @@ and cost-effective AI systems that drive innovation and efficiency across their 
         ],
     },
     'assets': {
+        'web.assets_backend': [
+            'ai_agent/static/src/js/quest_plugin.js',
+
+            'ai_agent/static/src/js/components/quest_dialog.js',
+            'ai_agent/static/src/js/components/quest_prompt_dialog.js',
+            'ai_agent/static/src/js/components/quest_prompt_dialog.xml',
+
+            'ai_agent/static/src/js/components/quest_selector_dialog.xml',
+            'ai_agent/static/src/js/components/quest_selector_dialog.js',
+        ]
     },
     "demo": [],
     "application": True,
