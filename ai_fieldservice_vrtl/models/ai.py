5026d64fd937f6ef9605c373da46426206971954
marcuszxc <marcuszxc10@gmail.com>
2025-03-10 17:28:56 +0000

    ai_agent:mer context

--- ai_fieldservice_vrtl/models/ai.py
+++ ai_fieldservice_vrtl/models/ai.py
@@ -27,8 +27,17 @@ class AIAgent(models.Model):
         if self.ai_type == "fieldservice-order":
             service_order = self.env['fieldservice.order'].search([('ai_quest_id', '=', quest.id)], limit=1)
             if service_order:
-                res['Service Order Number'] = service_order.name
+                res['Service Title'] = service_order.name
+                res['Reference number'] = service_order.order_number
                 res['Service Order Description'] = service_order.description
+                res['The manufacturer or brand of the product'] = service_order.brand
+                res['The model name or number of the product'] = service_order.model
+                res['The unique serial number of the product'] = service_order.serial_number
+                res['The product number or part number'] = service_order.product_number
+                res['Any specific marking or label on the product'] = service_order.marking
+                res['The date when the product was purchased'] = service_order.purchase_date
+                res['Work instructions'] = service_order.work_instructions
+                res['Directions to location'] = service_order.location_instructions
         return res
 
 class AIQuest(models.Model):
