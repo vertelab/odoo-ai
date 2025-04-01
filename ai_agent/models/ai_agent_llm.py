3d806a310169536d6773f31a8a32367d81086a82
vertelbot <vertelbot@vertel.se>
2025-04-01 13:42:17 +0000

    odoobranchpfile odoo-ai from 18.0. Do not p-file sync

--- ai_agent/models/ai_agent_llm.py
+++ ai_agent/models/ai_agent_llm.py
@@ -62,6 +62,7 @@ class AIAgentLLM(models.Model):
     status_color = fields.Integer(compute="compute_status_color")
     azure_endpoint = fields.Char(string="Azure Endpoint")
     api_version = fields.Char(string="API version")
+
     def action_get_quests(self):
         action = {
             'name': 'AI Quests',
