5e619e7c635df291d9e804d33c7e0e2488fbea91
vertelbot <vertelbot@vertel.se>
2025-04-09 14:00:20 +0000

    odoobranchpfile odoo-ai from 14.0. Do not p-file sync

--- ai_agent/__manifest__.py
+++ ai_agent/__manifest__.py
@@ -94,7 +94,13 @@ and cost-effective AI systems that drive innovation and efficiency across their 
     'website': "https://vertel.se/apps/odoo-ai/ai_agent",
     'images': ["static/description/banner.png"],  # 560x280
     "license": "AGPL-3",
-    "depends": ["mail", "product", "crm", "queue_job", "web_widget_mermaid_field"],
+    "depends": [
+        "mail",
+        "product",
+        "crm",
+        "queue_job",
+        "web_widget_mermaid_field"
+    ],
     "data": [
         "security/ai_agent_security.xml",
         "security/ir.model.access.csv",
@@ -111,7 +117,6 @@ and cost-effective AI systems that drive innovation and efficiency across their 
         "data/ai_agent_data.xml",
         "data/ai_tool_data.xml",
         "data/ollama_data.xml",
-        "demo/ai_agent_demo.xml",
         "wizard/ai_agent_test_wizard_views.xml",
         "wizard/ai_quest_test_mail_wizard_views.xml",
         "wizard/ai_memory_test_wizard_views.xml",
@@ -170,7 +175,9 @@ and cost-effective AI systems that drive innovation and efficiency across their 
             'ai_agent/static/src/js/components/quest_selector_dialog.js',
         ],
     },
-    "demo": [],
+    "demo": [
+        "demo/ai_agent_demo.xml",
+    ],
     "application": True,
     "installable": True,
     "auto_install": False,
