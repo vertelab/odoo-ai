bcd20c8ff06cb32dfe4f2a7c1e9ff39f4267365c
Anders Wallenquist <anders.wallenquist@vertel.se>
2025-05-05 11:28:41 +0000

    T/2248 changes how to get ai.tools

--- ai_agent/models/ai_quest_save_code.p.py
+++ ai_agent/models/ai_quest_save_code.p.py
@@ -3,8 +3,11 @@ from odoo import models, api
 from odoo.exceptions import UserError
 import base64
 import io
+import logging
 import re
 import zipfile
+
+_logger = logging.getLogger(__name__)
 
 class AiQuest(models.Model):
     _inherit = 'ai.quest'
@@ -35,6 +38,7 @@ class AiQuest(models.Model):
     
     @api.model
     def generate_simple_module(self, module_name):
+        attachments =  []
         xml_body = "\n<!-- Full records: Quest, agent memory and tools -->\n"
         obj = [self]
         for a in self.ai_agent_ids.mapped('ai_agent_id'):
@@ -44,8 +48,8 @@ class AiQuest(models.Model):
         for o in list(set(obj)):
             xml_body += self.to_xml(module_name,o)        
         
-        xml_body += "\n<!-- Glue records: agent to tools -->\n"
         for a in self.ai_agent_ids.mapped('ai_agent_id'):
+            xml_body += "\n<!-- Glue records: agent to tools -->\n"
             for t in a.ai_tool_ids.mapped('ai_tool_id'):
                 xml_body += f"""
         <record id="{a._name.replace('.', '_')}_{t._name.replace('.', '_')}_{t.id}" model="ai.agent.tool">
@@ -62,27 +66,27 @@ class AiQuest(models.Model):
             <field name="ai_memory_id" ref="{self.external_id(module_name, m)}"/>
         </record>\n
         """.strip()
-            attachments =  []
-            for att in self.env['ir.attachment'].search([
-                                    ('res_model', '=', m._name),
-                                    ('res_id', '=', m.id),
-                            ]):
-                # Skapa ett unikt id för bilagan
-                att_xml_id = f"{module_name}_attachment_{att.id}"
-                # Lägg till bilagan i xml_body (datas är base64)
-                xml_body += "\n" + f"""\n<record id="{att_xml_id}" model="ir.attachment">
-    <field name="name">{att.name}</field>
-    <field name="type">{att.type}</field>
-    <field name="datas" type="base64" file="{module_name}/files/{att.name}"/>
-    <field name="res_model">{att.res_model}</field>
-    <field name="res_id" ref="{self.external_id(module_name, m)}"/>
-    <field name="mimetype">{att.mimetype or ''}</field>
-</record>
+                for att in self.env['ir.attachment'].search([
+                                        ('res_model', '=', m._name),
+                                        ('res_id', '=', m.id),
+                                ]):
+                        # Skapa ett unikt id för bilagan
+                        att_xml_id = f"{module_name}_attachment_{att.id}"
+                        # Lägg till bilagan i xml_body (datas är base64)
+                        xml_body += "\n" + f"""\n<record id="{att_xml_id}" model="ir.attachment">
+        <field name="name">{att.name}</field>
+        <field name="type">{att.type}</field>
+        <field name="datas" type="base64" file="{module_name}/files/{att.name}"/>
+        <field name="res_model">{att.res_model}</field>
+        <field name="res_id" ref="{self.external_id(module_name, m)}"/>
+        <field name="mimetype">{att.mimetype or ''}</field>
+    </record>
 
-""".strip()
-            attachments.append((f"{module_name}/files/{att.name}",base64.b64decode(att.datas)))
-            if m.memory_faiss:
-                attachments.append((f"{module_name}/files/{m.name}.faiss",base64.b64decode(m.memory_faiss)))
+    """.strip()
+                        attachments.append((f"{module_name}/files/{att.name}",base64.b64decode(att.datas)))
+                if m.memory_faiss:
+                    attachments.append((f"{module_name}/files/{m.name}.faiss",base64.b64decode(m.memory_faiss)))
+            
             xml_body += "\n<!-- Glue records: quest to agent -->\n"
             for a in self.mapped('ai_agent_ids').mapped('ai_agent_id'):
                 xml_body += f"""
