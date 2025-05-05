f85828111cc0c90d68ec3eb9d5c4c8b4af933d5e
vertelbot <vertelbot@vertel.se>
2025-05-05 15:33:02 +0000

    odoobranchpfile odoo-ai from 18.0. Do not p-file sync T/2248

--- ai_agent/models/ai_quest.p.py
+++ ai_agent/models/ai_quest.p.py
@@ -179,7 +179,7 @@ class AIQuest(models.Model):
     ai_agent_ids = fields.One2many(comodel_name='ai.quest.agent', inverse_name='ai_quest_id')
     ai_type = fields.Selection(selection=[("default", "Default"), ('ai-programmer', 'AI Programmer')],
                                default="default", required=True)
-    alias_id = fields.Many2one(comodel_name='mail.alias', string='Alias', ondelete="restrict", required=True,
+    alias_id = fields.Many2one(comodel_name='mail.alias', string='Alias', ondelete="restrict", required=False,
                                help="The email address associated with this channel. New emails received will "
                                     "automatically create new leads assigned to the channel.")
     # #if VERSION <= "17.0"
