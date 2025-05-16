2234c383d95616a4f93c678cce98b7a9b34898eb
Anders Wallenquist <anders.wallenquist@vertel.se>
2025-05-16 14:47:16 +0000

    T/2430 Logo berget.ai and Ollama

---
+++ quest_module/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Quest Module",
+    'version': '1.0',
+    'depends': ['ai_agent'],
+    'author': "My Company",
+    'category': 'Tools',
+    'description': """
+Quest CodeOwl
+""",
+    'data': ['data/ai_quest.xml'],
+    'installable': True,
+    'application': False,
+}
