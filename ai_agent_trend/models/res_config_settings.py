2b9cc60bc0e4c5f2c2c0b9352a172cb1f879d73f
Anders Wallenquist <anders.wallenquist@vertel.se>
2025-06-26 15:02:56 +0000

    ai_agent:trend: cindug_settings

---
+++ ai_agent_trend/models/res_config_settings.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import ValidationError, UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    youtube_api_key = fields.Boolean(string="Youtube API Key", help="Keys are created at https://console.cloud.google.com", 
+                                config_parameter='ai_agent_trend.youtube_api_key')
+  
