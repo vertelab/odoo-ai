045cf992784d86c7f8eaf65ff5004dd444d2fd01
vertelbot <vertelbot@vertel.se>
2025-08-05 17:21:03 +0000

    odoobranchpfile odoo-ai from 18.0. Do not p-file sync

---
+++ ai_agent_mcp/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) {year} {company} (<{mail}>)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#
+# https://www.odoo.com/documentation/14.0/reference/module.html
+#
+
+{
+
+    'name': "odoo-ai: MCP Resource Management",
+    'version': "1.0",
+    'summary': "Adds MCP Power to AI Agent and AI Quests",
+    'category': "AI Orchestration",
+    "description": "Module to manage 'res.mcp' resources with interface and security rules.",
+    "author": "Vertel AB",
+    'website': "https://vertel.se/apps/odoo-ai/ai_agent",
+    'images': ["static/description/banner.png"],  # 560x280
+    "license": "AGPL-3",
+    "depends": ["fastapi", "ai_agent"],
+    'external_dependencies': {
+        'python': ['fastapi-mcp', 'fastmcp']
+    },
+    
+    "data": [
+        "security/ir.model.access.csv",
+        "security/res_mcp_security.xml",
+        "views/res_mcp_views.xml",
+        'views/ai_quest_view.xml',
+        'views/ai_tool_view.xml',
+    ],    
+    'post_init_hook': 'post_init_hook',
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
