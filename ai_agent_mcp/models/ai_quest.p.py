9189f0862e278e780e45cc8a4c8ea4bc288612b5
iammiracle <miracleaayodele@gmail.com>
2025-08-13 11:35:49 +0200

    added resources to mcp and more cleanup done

--- ai_agent_mcp/models/ai_quest.p.py
+++ ai_agent_mcp/models/ai_quest.p.py
@@ -1,3 +1,4 @@
+import re
 from odoo import models, fields, api
 
 
@@ -10,3 +11,28 @@ class AIQuestMCP(models.Model):
         help="Make this quest available through Model Context Protocol"
     )
 
+    # MCP
+    input_schema = fields.Text(
+        string="Input Schema",
+        help="JSON schema for the tool's input parameters, following the MCP specification.",
+        copy=False,
+    )
+    tool_name = fields.Char(
+        string="Tool Name",
+        copy=False,
+        help="Programmatic name for the tool. Auto-suggested, but can be edited.",
+    )
+
+    @api.onchange('name')
+    def _onchange_name_set_tool_name(self):
+        if self.name:
+            self.tool_name = re.sub(r'[^a-zA-Z0-9_]', '', self.name.lower().replace(' ', '_'))
+        else:
+            self.tool_name = ''
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('name') and not vals.get('tool_name'):
+                vals['tool_name'] = re.sub(r'[^a-zA-Z0-9_]', '', vals['name'].lower().replace(' ', '_'))
+        return super(AIQuestMCP, self).create(vals_list)
