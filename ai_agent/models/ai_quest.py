a76722c530cd848b084f9d2d02001a25edc75f8a
vertelbot <vertelbot@vertel.se>
2025-09-09 13:49:07 +0000

    odoobranchpfile odoo-ai from 18.0. Do not p-file sync

--- ai_agent/models/ai_quest.py
+++ ai_agent/models/ai_quest.py
@@ -244,7 +244,7 @@ class AIQuest(models.Model):
         help="Temperature controls the randomness and creativity of the model's output, "
              "<1.0 more predictable and consistent >1.0 more diverse and creative responses"
     )
-    company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
+    company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
     company_partner_id = fields.Many2one('res.partner', related="company_id.partner_id")
     allow_trigger_words = fields.Boolean(string="Use activation word")
     chat_trigger_words = fields.Text(string="Activation word", help="Separate words using commas")
