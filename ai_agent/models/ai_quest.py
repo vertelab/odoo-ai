39d2fd9ab32f1c0cb1b4cc9f2cb4a193fe44e73f
vertelbot <vertelbot@vertel.se>
2025-12-01 10:01:05 +0000

    odoobranchpfile odoo-ai from 18.0. Do not p-file sync

--- ai_agent/models/ai_quest.py
+++ ai_agent/models/ai_quest.py
@@ -12,6 +12,7 @@ import warnings
 import math
 
 from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 from IPython.display import Image, display
 from langchain import hub
 from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser, create_tool_calling_agent, \
@@ -408,9 +409,8 @@ class AIQuest(models.Model):
     @api.depends("session_line_ids")
     def compute_session_line_count(self):
         for record in self:
-            #record.session_line_count = sum([l.token_sys or 0 for l in record.session_line_ids])
-            start_date = date.today() - timedelta(days=date.today().day - 1)
-            end_date = date(year=date.today().year, month=date.today().month + 1, day=1) - timedelta(days=1)
+            start_date = date(year=date.today().year, month=date.today().month, day=1)
+            end_date = date(year=date.today().year, month=date.today().month, day=1) + relativedelta(months=1) - timedelta(days=1)
             filterd_line_ids = list(filter(lambda session_line_id: session_line_id.datetime.date() >= start_date and session_line_id.datetime.date() <= end_date, record.session_line_ids))
             record.session_line_count = sum([f.token_sys or 0 for f in filterd_line_ids])
 
