388dfc41126a510088bcc23dcea49b19675631ba
Anders Wallenquist <waland@gw0.vertel.se>
2026-07-23 09:40:41 +0000

    chore: remove accidentally committed pip packages from 03717e96

--- pgvector/asyncpg/__init__.py
+++
@@ -1,9 +0,0 @@
-from .register import register_vector
-from ..utils import Vector, HalfVector, SparseVector
-
-__all__ = [
-    'register_vector',
-    'Vector',
-    'HalfVector',
-    'SparseVector'
-]
